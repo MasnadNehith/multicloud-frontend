b83c90b97735f7620d9f48a45fce430ed07059af
masnad <masnad.nehith@gmail.com>
2020-04-11 16:12:20 +0300

    trasnfering over https

--- src/logic.js
+++ src/logic.js
@@ -1,7 +1,7 @@
 const failover = async () => {
     let URL = {
         aws : 'https://yyjfe9pdf5.execute-api.us-east-1.amazonaws.com',
-        azure : 'http://sls-neur-dev-multiclouddeploymentblog.azurewebsites.net'
+        azure : 'https://sls-neur-dev-multiclouddeploymentblog.azurewebsites.net'
     }
     try{
         const data = await fetch(`${URL.aws}/dev/status`);
